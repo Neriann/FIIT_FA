ba9170c17e17dde0d8d6b7798df69e9a65df814e
nerian <samslowpokd@gmail.com>
2026-04-03 23:58:57 +0300

    solve Splay

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -11,17 +11,109 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
     
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
+    {}
+    
+    protected override void RemoveNode(BstNode<TKey, TValue> node)
     {
-        throw new NotImplementedException();
+        Splay(node);
+
+        BstNode<TKey, TValue>? leftSubtree = Root?.Left;
+        BstNode<TKey, TValue>? rightSubtree = Root?.Right;
+
+        leftSubtree?.Parent = null;
+        rightSubtree?.Parent = null;
+
+        Root?.Left = null;
+        Root?.Right = null;
+
+        if (leftSubtree == null)
+        {
+            Root = rightSubtree;
+            return;
+        }
+
+        Root = leftSubtree;
+        int depth = 0;
+        BstNode<TKey, TValue> maxElement = GetMaximumElement(leftSubtree, ref depth);
+        
+        Splay(maxElement);
+        
+        Root.Right = rightSubtree;
+        rightSubtree?.Parent = Root;
     }
+
+    public override bool ContainsKey(TKey key)
+    {
+        return TryGetValue(key, out _);
+    }
+
+    private BstNode<TKey, TValue>? FindNode(TKey key, out BstNode<TKey, TValue>? prev)
+    {
+        prev = null;
+        
+        BstNode<TKey, TValue>? current = Root;
+        while (current != null)
+        {
+            int cmp = Comparer.Compare(key, current.Key);
+            if (cmp == 0)
+            {
+                return current;
+            }
+            prev = current;
+            current = cmp < 0 ? current.Left : current.Right;
+        }
+        return null;
+    }
+
     
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        BstNode<TKey, TValue>? target = FindNode(key, out BstNode<TKey, TValue>? prev);
+        if (target != null)
+        {
+            value = target.Value;
+            Splay(target);
+            return true;
+        }
+        value = default;
+        Splay(prev);
+        return false;
+    }
+
+    private void Splay(BstNode<TKey, TValue>? node)
+    {
+        while (node?.Parent != null)
+        {
+            if (node.Parent.Parent == null)
+            {
+                if (node.IsLeftChild) RotateRight(node.Parent);
+                else  RotateLeft(node.Parent);
+            }
+            // LL
+            else if (node.Parent.IsLeftChild && node.IsLeftChild)
+            {
+                RotateDoubleRight(node.Parent.Parent);
+            } 
+            // LR
+            else if (node.Parent.IsLeftChild && node.IsRightChild)
+            {
+                RotateBigRight(node.Parent.Parent);
+            }
+            // RR
+            else if (node.Parent.IsRightChild && node.IsRightChild)
+            {
+                RotateDoubleLeft(node.Parent.Parent);
+            }
+            // RL
+            else
+            {
+                RotateBigLeft(node.Parent.Parent);
+            }
+        }
     }
     
 }
